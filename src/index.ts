3350d9c9ed42ceda5061ac5c3fb5a3e08934179f
ayoub touba <ayubtouba@gmail.com>
2020-11-06 11:55:17 +0000

    Build ws /books/:id

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import express, { Request, Response } from "express";
 const app = express()
 
-
+var  books = [        
+    {id:1 ,name : "men's search for meaning ", nbrpages : "400"},
+    {id:2 ,name : "Sophie's world ", nbrpages : "500"},
+    {id:3 ,name : "The Stranger", nbrpages : "80"},
+ ]
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Welcome to Api Books");
 })
 
 app.get("/books", (req: Request, res: Response) => {
-    let books = [        
-        {name : "men's search for meaning ", nbrpages : "400"},
-        {name : "Sophie's world ", nbrpages : "500"},
-        {name : "The Stranger", nbrpages : "80"},
-     ]
+ 
     res.status(200).send(books);
   })
-
+  
+app.get("/books/:id", (req: Request, res: Response) => {
+   let book =  books.filter(bk => bk.id == +req.params.id) ? books.filter(bk => bk.id == +req.params.id) : null;
+    res.status(200).send(book);
+  })
 app.listen(8000,()=>{
   console.log('Server Started at Port, 8000')
 })
